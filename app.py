53e6c21d52ad22430ac498f79baf0bce4f128637
MikeBorman1 <mike.borman@yahoo.com>
2023-08-08 16:11:45 +0700

    d

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from langchain import PromptTemplate
+from langchain import PromptTemplate, LLMChain
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
@@ -16,8 +16,9 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from langchain.schema import SystemMessage
-from fastapi import FastAPI
-#import streamlit as st
+from twit import tweeter
+
+import streamlit as st
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
@@ -95,9 +96,10 @@ def summary(objective, content):
         separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
     docs = text_splitter.create_documents([content])
     map_prompt = """
-    Write a summary of the following text for {objective}. The text is Scraped data from a website so 
+    Extract the key information as summary of the following text for {objective}. The text is Scraped data from a website so 
     will have a lot of usless information that doesnt relate to this topic, links, other news stories etc.. 
-    Only summarise the relevant Info and try to keep as much factual information Intact:
+    Only summarise the relevant Info and try to keep as much factual information Intact
+    Do not describe what the webpage is, you are here to get acurate and specific information:
     "{text}"
     SUMMARY:
     """
@@ -146,7 +148,7 @@ tools = [
 ]
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
             you do not make things up, you will try as hard as possible to gather facts & data to back up the research
             
             Please make sure you complete the objective above with the following rules:
@@ -155,7 +157,19 @@ system_message = SystemMessage(
             3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
             4/ You should not make things up, you should only write facts & data that you have gathered
             5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            6/ Always look at the web first if possible
+            7/ Output as much information as possible
+            8/ Be specific about your reasearch, do not just point to a website and say things can be found here, that what you are for
+            
+
+            Example of what NOT to do return, these tell the user nothing!!
+
+            1/WIRED - WIRED provides the latest news, articles, photos, slideshows, and videos related to artificial intelligence. Source: WIRED
+
+            2/Artificial Intelligence News - This website offers the latest AI news and trends, along with industry research and reports on AI technology. Source: Artificial Intelligence News
+
+            3/MIT News - Massachusetts Institute of Technology (MIT) provides news articles related to artificial intelligence. One recent article discusses an AI model that can help determine the origin of a patient's cancer. Source: MIT News
+            """
 )
 
 agent_kwargs = {
@@ -176,8 +190,63 @@ agent = initialize_agent(
     memory=memory,
 )
 
-'''
-# 4. Use streamlit to create a web app
+
+template = """
+    You are a very experienced ghostwriter who excels at writing Twitter threads.
+You will be given a bunch of info below and a topic headline, your job is to use this info and your own knowledge
+to write an engaging Twitter thread.
+The first tweet in the thread should have a hook and engage with the user to read on.
+Example hooks:
+1.____ simple principles to_______
+
+2.____ sentences that'll make you _____ than a _______ degree
+3._____ ____ rules everyone should learn by ____
+4.This will make you ______ in __ days
+5.If you want to fix____________, read this
+6.__ things I know at __ I wish I’d known at ___
+7.It feels illegal to know about these______________
+8. ___ habits to get rid of_______
+9.Here are __ ______ Shortcuts that'll save you hundreds of hours of your life
+10._____ people use _____ every day. But no one uses it effectively.
+11. Most people don't understand _____, but its critical too _______. Here's it explained simply...
+
+The twitter thread should be between the length of 3 and 10 tweets 
+Each tweet should start with (tweetnumber/total length)
+Dont overuse hashtags, only one or two for entire thread.
+Use links sparingly and only when really needed, but when you do make sure you actually include them! 
+Only return the thread, no other text
+Make sure each tweet is lower that 220 chars
+    Topic Headline:{topic}
+    Info: {info}
+    """
+
+prompt = PromptTemplate(
+    input_variables=["info","topic"], template=template
+)
+
+llm = ChatOpenAI()
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=True,
+    
+)
+
+  
+twitapi = tweeter()
+
+def tweetertweet(thread):
+    tweets = thread.split("\n\n")
+    response = twitapi.create_tweet(text=tweets[0])
+    id = response.data['id']
+    tweets.pop(0)
+    for i in tweets:
+        reptweet = twitapi.create_tweet(text=i, 
+                                    in_reply_to_tweet_id=id, 
+                                    )
+        id = reptweet.data['id']
+    
+
 def main():
     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
 
@@ -188,25 +257,14 @@ def main():
         st.write("Doing research for ", query)
 
         result = agent({"input": query})
-
-        st.info(result['output'])
-
+        thread = llm_chain.predict(topic = query, info = result['output'])
+        st.markdown(thread)
+        tweet =  st.button("Tweeeeeet")
+        st.markdown("Twitter thread Generated from the below research")
+        st.markdown(result['output'])
+        if tweet:
+            tweetertweet(thread)
 
 if __name__ == '__main__':
     main()
-'''
 
-# 5. Set this as an API endpoint via FastAPI
-app = FastAPI()
-
-
-class Query(BaseModel):
-    query: str
-
-
-@app.post("/")
-def researchAgent(query: Query):
-    query = query.query
-    content = agent({"input": query})
-    actual_content = content['output']
-    return actual_content
